648bb642339b3675dfeb56be03d0f7d7f65003b5
oleg <olegapril86@mail.ru>
2015-09-08 16:44:03 +0600

    8.09.15 new version

--- MP4/scripts/my_scripts.js
+++ MP4/scripts/my_scripts.js
@@ -4,14 +4,26 @@
 $(document).ready(function(){
     var v = false;
     $("button#vegOn").click(function(){
-        $("li.fish").detach();
         if (v == false){
+            $f = $("li.fish").parent().parent().detach();
+            $(".hamburger").replaceWith("<li class='portobello'><em>Portobello Mashroom</em></li>");
+            $(".meat").after("<li class='tofu'><em>Tofu</em></li>");
+            $m = $(".meat").detach();
+            $(".tofu").parent().parent().addClass("veg_leaf");
+            $(".portobello").parent().parent().addClass("veg_leaf");
             v = true;
-
         }
     });
     $("button#restoreMe").click(function(){
         if(v == true){
+            $(".portobello").replaceWith("<li class='hamburger'>hamburger</li>");
+            $(".menu_entrees li").first().before($f);
+            $(".tofu").each(function(i){
+                $(this).after($m[i]);
+            });
+            $(".tofu").remove();
+            $(".meat").parent().parent().removeClass("veg_leaf");
+            $(".hamburger").parent().parent().removeClass("veg_leaf");
             v = false;
         }
     });
