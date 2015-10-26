7940a5f9c07cbddefa4c1350d65e6eee191b0ad1
oleg <olegapril86@mail.ru>
2015-10-26 18:50:08 +0600

    parse.com connected

--- reactBSTable/app.js
+++ reactBSTable/app.js
@@ -5,8 +5,10 @@ var Item = React.createClass({
 		)
 	}
 })
+var GroupObject = Parse.Object.extend("ERFGROUP");;
 
 var MyComponent = React.createClass({
+
 	getInitialState: function(){
 		return {data:[]}
 	},
@@ -19,24 +21,60 @@ var MyComponent = React.createClass({
 			return;
 		}
 		this.handleSubmit({code: code, name: name, desc: desc});
+
+		//*Parse BLOCK*/
+
+		var groupObject = new GroupObject();
+		groupObject.save({Code: code,Name: name, Desc: desc}, {
+			success: function(object) {
+				$(".success").show();
+			},
+			error: function(model, error) {
+				console.log("Error:" + error.message );
+			}
+		});
+
 		React.findDOMNode(this.refs.fCode).value = '';
 		React.findDOMNode(this.refs.fName).value = '';
 		React.findDOMNode(this.refs.fDesc).value = '';
 	},
 
-	loadFromServer: function() {
-		$.ajax({
-			url: this.props.url,
-			dataType: 'json',
-			cache: false,
-			success: function(data) {
-				this.setState({data: data});
-			}.bind(this),
-			error: function(xhr, status, err) {
-				console.error(this.props.url, status, err.toString());
-			}.bind(this)
+	loadFromParse: function(){
+		const data = this.state.data;
+		var query = new Parse.Query(GroupObject);
+		query.find({
+			success: function(results){
+				var dataRows = data;
+				var newDataRows;
+				for (var i in results){
+					var code = results[i].get("Code");
+					var name = results[i].get("Name");
+					var desc = results[i].get("Desc");
+					newDataRows = dataRows.concat({code:code,name:name,desc:desc});
+					console.log("FG CODE: " + code);
+				}
+				this.setState({data: newDataRows});
+				//this.setState({data: results});
+			}.bind(this),error: function(error){
+				console.log("Query Error:" + error.message)
+		````}
 		});
+		//this.setState({data:data});
 	},
+
+	//loadFromServer: function() {
+	//	$.ajax({
+	//		url: this.props.url,
+	//		dataType: 'json',
+	//		cache: false,
+	//		success: function(data) {
+	//			this.setState({data: data});
+	//		}.bind(this),
+	//		error: function(xhr, status, err) {
+	//			console.error(this.props.url, status, err.toString());
+	//		}.bind(this)
+	//	});
+	//},
 	handleSubmit: function(row) {
 		var dataRows = this.state.data;
 		var newDataRows = dataRows.concat([row]);
@@ -55,7 +93,9 @@ var MyComponent = React.createClass({
 		});
 	},
 	componentWillMount: function() {
-		this.loadFromServer();
+		Parse.initialize("RLodcdOxlKYYPTmLZ4sofv3M5R3XKG7d7qZE6XBe", "IoKsmB6f7HbdvkzXLpCHn2JbYrqjMev7HTFYaq4r");
+		//this.loadFromServer();
+		this.loadFromParse();
 		//setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	},
 	//render: function () {
@@ -77,12 +117,24 @@ var MyComponent = React.createClass({
 
 	render: function () {
 		const data = this.state.data;
+
+
+
+
 		return(
-			<BootstrapTable data={data} height="120" striped={true} hover={true}>
-				<TableHeaderColumn dataField="code" isKey={true} dataAlign="center" dataSort={true}>Code</TableHeaderColumn>
-				<TableHeaderColumn dataField="name" dataSort={true}>Name</TableHeaderColumn>
-				<TableHeaderColumn dataField="desc">Description</TableHeaderColumn>
-			</BootstrapTable>
+			<div>
+				<input type="input" placeholder = "Type fg code" ref="fCode"/>
+				<input type="input" placeholder = "Feed group name " ref="fName"/>
+				<input type="input" placeholder = "Description" ref="fDesc"/>
+				<input type="button" value = "Submit" onClick = {this.handleOnClick}/>
+				<div>
+					<BootstrapTable data={data} height="190" striped={true} hover={true}>
+						<TableHeaderColumn dataField="code" isKey={true} dataAlign="center" dataSort={true}>Code</TableHeaderColumn>
+						<TableHeaderColumn dataField="name" dataSort={true}>Name</TableHeaderColumn>
+						<TableHeaderColumn dataField="desc">Description</TableHeaderColumn>
+					</BootstrapTable>
+				</div>
+			</div>
 		)
 	}
 })
