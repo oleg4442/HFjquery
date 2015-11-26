a983a0fd34b5b6fb40c74f46f4300f64d7596ebd
oleg <olegapril86@mail.ru>
2015-11-26 17:53:37 +0600

    true

--- billing/app.js
+++ billing/app.js
@@ -1,4 +1,6 @@
-var contract = Parse.Object.extend("Contract");
+var Contract = Parse.Object.extend("Contract");
+var datePattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+
 var ModalTrigger = React.createClass(
 	{ handleClick: function(e) {
 		$(this.refs.payload.getDOMNode()).modal(); }
@@ -66,13 +68,13 @@ var MyComponent = React.createClass({
 	},
 
 	loadFromParse: function(){
-		var query = new Parse.Query(contract);
+		var query = new Parse.Query(Contract);
 		console.log("loadFromParse");
 		query.find({
 			success: function(results){
 				var newDataRows = [];
 				for (var i in results){
-					var startDate = results[i].get("startDate").toDateString()
+					var startDate = results[i].get("startDate").toDateString();
 					var person = results[i].get("person");
 					var project = results[i].get("project");
 					var rateHourly = results[i].get("rateHourly");
@@ -98,22 +100,56 @@ var MyComponent = React.createClass({
 
 	handleOnClick: function(e) {
 		e.preventDefault();
-		const staff = React.findDOMNode(this.refs.person).value.trim();
+		const staff = parseInt(React.findDOMNode(this.refs.person).value.trim());
+		const prj = parseInt(React.findDOMNode(this.refs.project).value.trim());
+		const stdate_str = React.findDOMNode(this.refs.start_date).value.trim();
+		const rate1 = parseInt(React.findDOMNode(this.refs.rate_hourly).value.trim());
+		const rate2 = parseInt(React.findDOMNode(this.refs.rate_monthly).value.trim());
+		const curr = React.findDOMNode(this.refs.currency).value.trim();
+		const of = parseInt(React.findDOMNode(this.refs.overtime_factor).value.trim());
+
+		const stdate = new Date((React.findDOMNode(this.refs.start_date).value).replace(datePattern,'$3-$2-$1'));
 		console.log("Person = " + staff);
+		console.log("Project = " + prj);
+		console.log("Start Date = " + stdate);
 		console.log("Clicked!");
-		//var person = prompt("Please enter your name", "Harry Potter");
-		//
-		//if (person != null) {
-		//	console.log("Hello " + person + "! How are you today?");
-		//}
+		if (!staff || !prj) {
+			return;
+		}
+		this.handleSubmit({person:staff,project:prj,startDate:stdate_str,rateHourly:rate1,rateMonthly:rate2,currency:curr,overtimeFactor:of});
+
+		//*Parse BLOCK*/
+
+		var contract = new Contract();
+		contract.save({person:staff,project:prj,startDate:stdate,rateHourly:rate1,rateMonthly:rate2,currency:curr,overtimeFactor:of}, {
+			success: function() {
+			},
+			error: function(model, error) {
+				console.log("Error:" + error.message );
+			}
+		});
+
+		React.findDOMNode(this.refs.person).value = '';
+		React.findDOMNode(this.refs.project).value = '';
+	},
+
+	handleSubmit: function(row){
+		var dataRows = this.state.data;
+		var newDataRows = dataRows.concat([row]);
+		this.setState({data: newDataRows});
 	},
 
 	render: function () {
 		const data = this.state.data;
 		console.log("rendering");
+		var selectRowProp = {
+			mode: "radio",
+			clickToSelect: true,
+			bgColor: "rgb(179, 209, 255)"
+		};
 		return(
 			<div>
-				<BootstrapTable data={data} height="250" striped={true} hover={true}>
+				<BootstrapTable data={data} height="250" striped={true} hover={true} selectRow={selectRowProp}>
 					<TableHeaderColumn dataField="startDate">Start Date</TableHeaderColumn>
 					<TableHeaderColumn dataField="person" isKey={true}>Person</TableHeaderColumn>
 					<TableHeaderColumn dataField="project" dataSort={true}>Project</TableHeaderColumn>
@@ -138,8 +174,6 @@ var MyComponent = React.createClass({
                    }
 				/>
 			</div>
-
-
 		)
 	}
 });
